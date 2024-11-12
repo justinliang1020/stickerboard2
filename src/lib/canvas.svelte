66a481d8ab723ea868db5fc1c3542eda7c9a00fb
Justin Liang <justinliang1020@gmail.com>
2024-11-12 09:19:06 -0500

    (in prog) initial draggable merge into canvas refactoring

--- src/lib/canvas.svelte
+++ src/lib/canvas.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-	import Draggable from '$lib/draggable.svelte';
 	import type { MediaFormat } from '$lib/types';
-	import type { ComponentProps } from 'svelte';
 	import Taskbar from '$lib/taskbar.svelte';
-	type DraggablePropTypes = ComponentProps<Draggable>;
 
-	let draggables: DraggablePropTypes[] = $state([]);
+	let draggables: Draggable[] = $state([]);
+
+	type Draggable = {
+		mediaFormat: MediaFormat;
+		src: string;
+		x: number;
+		y: number;
+		z: number;
+		width: number;
+		height: number;
+		isSelected: boolean;
+		isEditing: boolean;
+		isResizing: boolean;
+		isDragging: boolean;
+		imgEl: HTMLImageElement | null;
+		textAreaEl: HTMLTextAreaElement | null;
+		containerEl: HTMLDivElement | null;
+		activeCorner: string | null;
+	};
 
 	addDraggable('img', 'rat-spinning.gif');
 	addDraggable('img', 'frieren-icegif-5.gif', 500, 400);
@@ -25,9 +40,16 @@
 			x,
 			y,
 			z: z,
+			width: 100,
+			height: 100,
 			isSelected: false,
 			isEditing: false,
-			modifyZIndex: ''
+			isDragging: false,
+			isResizing: false,
+			imgEl: null,
+			textAreaEl: null,
+			containerEl: null,
+			activeCorner: null
 		});
 	}
 
@@ -125,39 +147,156 @@
 		}
 	}
 
-	$effect(() => {
-		// uncomment for debugging
-		// $inspect(draggables);
+	const minImageSize = 50; // pixels
+	let mouseDownStartX: number;
+	let mouseDownStartY: number;
+	let mouseDownStartWidth: number;
+	let mouseDownStartHeight: number;
 
-		//TODO: fix bad hack
-		draggables.map((d) => {
-			switch (d.modifyZIndex) {
-				case 'sendToBack':
-					d.z = getMinZIndex() - 1;
-					d.modifyZIndex = '';
+	function handleMouseDown(event: MouseEvent, d: Draggable) {
+		const target = event.target as HTMLElement;
+		if (target.classList.contains('resize-handle')) {
+			d.isDragging = false;
+			d.isResizing = true;
+			d.activeCorner = target.dataset.corner || null;
+			mouseDownStartX = event.clientX;
+			mouseDownStartY = event.clientY;
+			mouseDownStartWidth = d.width;
+			mouseDownStartHeight = d.height;
+		} else {
+			d.isDragging = true;
+			mouseDownStartX = event.clientX - d.x;
+			mouseDownStartY = event.clientY - d.y;
+		}
+		d.isSelected = true;
+		if (!d.isDragging && !d.isResizing) {
+			d.isSelected = !d.isSelected;
+		}
+	}
+
+	function clamp(x: number, y: number) {
+		return Math.max(x, y);
+	}
+
+	function handleMouseMove(event: MouseEvent) {
+		const d: Draggable | null = draggables.find((d) => d.isSelected) || null;
+		if (d === null) {
+			return;
+		}
+		if (d.isDragging) {
+			d.x = event.clientX - mouseDownStartX;
+			d.y = event.clientY - mouseDownStartY;
+		} else if (d.isResizing && d.activeCorner) {
+			const dx = event.clientX - mouseDownStartX;
+			const dy = event.clientY - mouseDownStartY;
+			let ratio: number;
+			const originalWidth = d.width;
+			const originalHeight = d.height;
+
+			switch (d.activeCorner) {
+				case 'nw':
+					ratio = Math.sqrt(
+						(clamp(mouseDownStartWidth - dx, 0) / mouseDownStartWidth) *
+							(clamp(mouseDownStartHeight - dy, 0) / mouseDownStartHeight)
+					);
+					d.width = clamp(mouseDownStartWidth * ratio, minImageSize);
+					d.height = clamp(mouseDownStartHeight * ratio, minImageSize);
+					d.x -= d.width - originalWidth;
+					d.y -= d.height - originalHeight;
 					break;
-				case 'sendToFront':
-					d.z = getMaxZIndex() + 1;
-					d.modifyZIndex = '';
+				case 'ne':
+					ratio = Math.sqrt(
+						(clamp(mouseDownStartWidth + dx, 0) / mouseDownStartWidth) *
+							(clamp(mouseDownStartHeight - dy, 0) / mouseDownStartHeight)
+					);
+					d.width = clamp(mouseDownStartWidth * ratio, minImageSize);
+					d.height = clamp(mouseDownStartHeight * ratio, minImageSize);
+					d.y -= d.height - originalHeight;
+					break;
+				case 'sw':
+					ratio = Math.sqrt(
+						(clamp(mouseDownStartWidth - dx, 0) / mouseDownStartWidth) *
+							(clamp(mouseDownStartHeight + dy, 0) / mouseDownStartHeight)
+					);
+					d.width = clamp(mouseDownStartWidth * ratio, minImageSize);
+					d.height = clamp(mouseDownStartHeight * ratio, minImageSize);
+					d.x -= d.width - originalWidth;
+					break;
+				case 'se':
+					ratio = Math.sqrt(
+						(clamp(mouseDownStartWidth + dx, 0) / mouseDownStartWidth) *
+							(clamp(mouseDownStartHeight + dy, 0) / mouseDownStartHeight)
+					);
+					d.width = clamp(mouseDownStartWidth * ratio, minImageSize);
+					d.height = clamp(mouseDownStartHeight * ratio, minImageSize);
 					break;
 			}
-		});
-	});
+		}
+	}
+
+	function handleMouseUp() {
+		// TODO: unsure
+		for (const d of draggables) {
+			d.isDragging = false;
+			d.isResizing = false;
+			d.activeCorner = null;
+		}
+	}
+
+	function handleWindowMouseDown(event: MouseEvent) {
+		// TODO: fix
+		// const target = event.target as HTMLElement;
+		// if (target.closest('.draggable-container') !== containerEl) {
+		// 	isSelected = false;
+		// }
+	}
 </script>
 
-<svelte:window on:paste={handlePaste} on:keydown={handleKeyDown} />
+<svelte:window
+	on:paste={handlePaste}
+	on:keydown={handleKeyDown}
+	on:mousemove={handleMouseMove}
+	on:mouseup={handleMouseUp}
+	on:mousedown={handleWindowMouseDown}
+/>
 
 {#each draggables as d (d)}
-	<Draggable
-		mediaFormat={d.mediaFormat}
-		src={d.src}
-		bind:x={d.x}
-		bind:y={d.y}
-		bind:z={d.z}
-		bind:isSelected={d.isSelected}
-		bind:isEditing={d.isEditing}
-		bind:modifyZIndex={d.modifyZIndex}
-	/>
+	<!-- svelte-ignore a11y_no_static_element_interactions -->
+	<div
+		class="draggable-container"
+		class:selected={d.isSelected}
+		style="position: absolute; left: {d.x}px; top: {d.y}px; z-index: {d.z}; width: {d.width}px; height: {d.height}px;"
+		style:cursor="grab"
+		onmousedown={(e) => handleMouseDown(e, d)}
+		bind:this={d.containerEl}
+	>
+		{#if d.mediaFormat === 'img'}
+			<img
+				src={d.src}
+				alt={d.src.substring(0, 50)}
+				class="draggable-image"
+				draggable="false"
+				bind:this={d.imgEl}
+			/>
+		{:else if d.mediaFormat === 'text'}
+			<textarea
+				name=""
+				id=""
+				class="text"
+				bind:this={d.textAreaEl}
+				onfocusin={() => (d.isEditing = true)}
+				onfocusout={() => (d.isEditing = false)}>{d.src}</textarea
+			>
+		{/if}
+
+		{#if d.isSelected}
+			<!-- TODO: add back send to front / back functionality-->
+			<div class="resize-handle nw" data-corner="nw"></div>
+			<div class="resize-handle ne" data-corner="ne"></div>
+			<div class="resize-handle sw" data-corner="sw"></div>
+			<div class="resize-handle se" data-corner="se"></div>
+		{/if}
+	</div>
 {/each}
 
 <Taskbar {addDraggable} {processFileAndAddDraggable} />
@@ -166,5 +305,92 @@
 	:global(body) {
 		background: no-repeat fixed url('windows-xp-background.jpg') center / 100% 100%;
 		overflow: hidden;
+	}
+	.draggable-container {
+		display: inline-block;
+		user-select: none;
+		position: relative;
+		border: 3px solid transparent;
+	}
+
+	.draggable-image {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	.selected {
+		border-color: #001397;
+	}
+
+	.send-to-front {
+		position: absolute;
+		top: -30px;
+		left: 5px;
+	}
+
+	.send-to-back {
+		position: absolute;
+		top: -30px;
+		right: 5px;
+	}
+
+	.resize-handle {
+		position: absolute;
+		width: 10px;
+		height: 10px;
+		background: white;
+		border: 2px solid #3b82f6;
+		border-radius: 50%;
+	}
+
+	.nw {
+		top: -5px;
+		left: -5px;
+		cursor: nw-resize;
+	}
+
+	.ne {
+		top: -5px;
+		right: -5px;
+		cursor: ne-resize;
+	}
+
+	.sw {
+		bottom: -5px;
+		left: -5px;
+		cursor: sw-resize;
+	}
+
+	.se {
+		bottom: -5px;
+		right: -5px;
+		cursor: se-resize;
+	}
+
+	.text {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+
+		color: white;
+		font-size: 2em; /* Adjust size as needed */
+		text-align: center;
+		/* Outline effect */
+		text-shadow:
+			-1px -1px 0 #000,
+			1px -1px 0 #000,
+			-1px 1px 0 #000,
+			1px 1px 0 #000;
+		font-family: Tahoma, sans-serif;
+
+		background-color: transparent;
+		border: none;
+		outline: none;
+		resize: none;
+		padding: 0;
+		/* fix overflow to show without scrollbar */
+		overflow: hidden;
+		line-height: inherit;
 	}
 </style>
